75cce4b70c37f72dffe76d52cdce71b893eea763
Kananek T <info.dvgamer@gmail.com>
2019-02-07 01:09:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { debuger, Raven } from '@touno-io/debuger'
 import request from 'request-promise'
+
 const apiEndpoint = region => `http://air4thai.pcd.go.th/services/getNewAQI_JSON.php?region=${region}`
 const regionId = [ 1, 2, 3, 4, 5, 6, 7 ]
 const logger = debuger.scope('pollution')
+
 Raven.install({
 }).Tracking(async pkg => {
   logger.start(`data air4thai.pcd.go.th`)
