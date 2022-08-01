da151d55cbc81ca7c27933194fd1c00094dff1a4
Kananek Thongkam <info.dvgamer@gmail.com>
2022-08-01 23:17:54 +0700

    [Add] eslint supported

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     "extends": "eslint:recommended",
     "globals": {
       "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+      "SharedArrayBuffer": "readonly",
+      "describe": "readonly",
+      "it": "readonly",
     },
     "parserOptions": {
       "ecmaVersion": 2018
