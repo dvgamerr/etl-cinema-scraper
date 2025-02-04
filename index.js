52e41936f4f7e1a49f3e6e3755320a490cdf5c86
Kananek Thongkam <info.dvgamer@gmail.com>
2025-02-04 22:11:24 +0700

    refactor: remove unused LINEFlexRequest function and line-flex utility

--- index.js
+++ index.js
@@ -7,21 +7,9 @@ import weekOfYear from 'dayjs/plugin/weekOfYear'
 import { JSONWrite } from './untils/collector'
 import cinemaScraper from './plugins/cinema-scraper'
 import { name, version } from './package.json'
-// import flexCarousel from "./untils/line-flex"
 
 dayjs.extend(weekday)
 dayjs.extend(weekOfYear)
-
-// async function LINEFlexRequest(message, items) {
-//   const res = await fetch(`http://notice.touno.io/line/popcorn/movie`, {
-//     method: "PUT",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(flexCarousel(message, items)),
-//   })
-
-//   const body = await res.json()
-//   logger.info(`notice.touno.io (${res.status}): ${JSON.stringify(body)}`)
-// }
 
 let rawItems = []
 if (parseArgs.dryrun === true) {
@@ -71,53 +59,3 @@ for (const chunks of cinemaChunks) {
   }
 }
 logger.info(`Uploaded`)
-
-// logger.info(`${normalizeItems.length} movies normalizer`)
-
-// const collector = await fetch(`${Bun.env.APIS}/api/collector/cinema`, {
-//   method: "PUT",
-//   headers: {
-//     "Content-Type": "application/json",
-//     Authorization: `Bearer ${Bun.env.TOKEN}`,
-//   },
-//   body: JSON.stringify(cinemaItems),
-// })
-
-// if (collector.status !== 200) {
-//   const body = await collector.json()
-//   logger.error(`Uploaded: ${JSON.stringify(body)}`)
-//   Deno.exit()
-// } else {
-//   logger.info(" * Uploaded")
-// }
-
-// if (dayjs().day() !== 1) {
-//   Deno.exit()
-// }
-
-// const res = await fetch(`${Bun.env.APIS}/api/cinema`, {
-//   method: "GET",
-//   headers: {
-//     "Content-Type": "application/json",
-//     Authorization: `Bearer ${Bun.env.TOKEN}`,
-//   },
-// })
-
-// const body = await res.json()
-// if (res.status !== 200) {
-//   logger.error(`Cinema: ${JSON.stringify(body)}`)
-//   Deno.exit()
-// }
-
-// const limitFlex = 10
-// const groupFlex = Math.ceil(body.length / limitFlex)
-// logger.info(`LINE Flex ${groupFlex} sacle`)
-
-// for (let i = 0; i < groupFlex; i++) {
-//   await LINEFlexRequest(
-//     `ป๊อปคอนขอเสนอ โปรแกรมหนังประจำสัปดาห์ที่ ${dayjs().week()} ปี ${dayjs().year()}${
-//       groupFlex > 1 ? ` [${i}/${groupFlex}]` : ""
-//     } ครับผม`,
-//     body.slice(limitFlex * i, limitFlex * (i + 1))
-//   )
-// }
